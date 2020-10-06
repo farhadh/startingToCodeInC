7515e92e705925ef2d6caccff5aca93f33984652
Farhad <Farhad.21.7@gmail.com>
2020-10-06 14:35:36 +0200

    Getting input from the user

---
+++ inputFromUser.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+#define pi acos(-1.0);
+
+int main()
+{
+	int r;
+	
+	printf("Please enter the radius: ");
+	scanf("%i", &r);
+	
+/* scanf("%d", &b);
+The program will read in an integer value that the user enters on the keyboard (%d is for integers, as is printf, so b must be declared as an int) and place that value into b. */
+	
+	float area = r*r*pi;
+	printf("The circle's area is %f :)\n", area);
+
+	return 0;
+}
