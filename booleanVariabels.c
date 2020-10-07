4b4dd4c3fef107a4e344dd225d4335bc091e7cb2
Farhad <Farhad.21.7@gmail.com>
2020-10-07 15:37:08 +0200

    Learned about stdbool.h library

--- booleanVariabels.c
+++ booleanVariabels.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main()
 {
 	_Bool x = 1; // if 0: False any other numbers are True
-	printf("%i\n", x); // 5i becasue boolean values are integers
 
+	bool y = false; // this is from stdbool.h
+
+	printf("%i\n", x); // %i becasue boolean values are integers
+
+	printf("%i\n", y); // %i becasue boolean values are intege    rs
 
 	return 0;
 }
