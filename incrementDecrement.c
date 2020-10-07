416591cfcb5fb410f99ae7e1cd4c15f2231f6905
Farhad <Farhad.21.7@gmail.com>
2020-10-07 16:41:35 +0200

    Learned about how to increment/decrement
    Note that how it works when it gets assigned to a variable ;)

---
+++ incrementDecrement.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	int pizzasToEat = 123;
+	
+	int output = ++pizzasToEat; /* unary operator if you would like to have the incremented number of pizzas assinged to the out put, then ++ should come first, otherwise the pizzas get increased by one after it got assigned to the output viariable */
+
+	printf("%i\n", output);
+
+
+	return 0;
+}
