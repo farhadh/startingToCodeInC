93ac447f7f3a60ba889c0ee8ad6c7d4b7ae56e51
Farhad <Farhad.21.7@gmail.com>
2020-10-08 12:58:16 +0200

    Added arrays feature

--- incrementDecrement.c
+++ incrementDecrement.c
@@ -2,18 +2,24 @@
 
 int main()
 {
-	int x = 123;
+	int i;
+	double x[4];
+	for(i=0;i<4;i++)
+	{
+		x[i]=123;
+	}
 	
-	int output = ++x; /* unary operator if you would like to have the incremented number of x assigned to the out put, then ++ should come first, otherwise the x get increased by one after it got assigned to the output variable */
+	/* int output = ++x; – unary operator if you would like to have the incremented number of x assigned to the out put, then ++ should come first, otherwise the x get increased by one after it got assigned to the output variable */
 
-	/*
-	   x -= 10;
-	   x += 10;
-	   x /= 10;
-	   x %=10;
-	*/
-
-	printf("%i\n", output);
+	x[0] -= 10;
+	x[1] += 10;
+	x[2] /= 10;
+	x[3] = (int) x[3] % 10;
+	
+	for(i=0;i<4;i++)
+	{
+		printf("%f\n", x[i]);
+	}
 
 
 	return 0;
