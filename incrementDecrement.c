2d4648b7d6a3b5d87add5fd8802cc1cfd0834365
Farhad <Farhad.21.7@gmail.com>
2020-10-08 14:19:53 +0200

    .

--- incrementDecrement.c
+++ incrementDecrement.c
@@ -8,14 +8,14 @@ int main()
 	{
 		x[i]=123;
 	}
-	
+
 	/* int output = ++x; – unary operator if you would like to have the incremented number of x assigned to the out put, then ++ should come first, otherwise the x get increased by one after it got assigned to the output variable */
 
 	x[0] -= 10;
 	x[1] += 10;
 	x[2] /= 10;
 	x[3] = (int) x[3] % 10;
-	
+
 	for(i=0;i<4;i++)
 	{
 		printf("%f\n", x[i]);
