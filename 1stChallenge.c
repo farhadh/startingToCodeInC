6331ff2e75da1beae72eb638b704753c6376be99
Farhad <Farhad.21.7@gmail.com>
2020-10-14 15:26:08 +0200

    Caleb's first C coding challenge

---
+++ 1stChallenge.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	double a,b,c;
+
+	printf("Please enter the length of the 1st side: ");
+	scanf("%lf", &a);
+
+	printf("Please enter the length of the 2nd side: ");
+	scanf("%lf", &b);
+
+	c = sqrt(pow(a,2) + pow(b,2));
+	printf("The length of the hypotenuse is %f.\n", c);
+
+	return 0;
+}
