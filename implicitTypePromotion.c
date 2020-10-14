7f261504c59c24b9b7592e9f9d9a5d88651aefa1
Farhad <Farhad.21.7@gmail.com>
2020-10-14 13:54:53 +0200

    Learned about type promotions

---
+++ implicitTypePromotion.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+	
+	float x = 50; // x is defined as a float
+	
+	printf("%f\n", x); // printf function converts x to double type – x will get promoted
+
+	return 0;
+}
