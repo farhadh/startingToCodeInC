67caf53a2cd8b494bec7a8dcfc2804dc6fe22f6a
Farhad <Farhad.21.7@gmail.com>
2020-10-14 14:17:09 +0200

    Learned about type casting

---
+++ typeCasting.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+
+	int slices = 17;
+	int people = 2;
+	
+	double halfThePizza = (double) slices / people;
+	
+	printf("%f\n", halfThePizza);
+
+
+	return 0;
+}
