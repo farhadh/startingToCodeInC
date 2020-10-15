f2b0fd6efba63693e81a918322683eeb0b1ef2c2
Farhad <Farhad.21.7@gmail.com>
2020-10-15 15:20:26 +0200

    Ternary operator

---
+++ ternaryOperator.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+
+	double x = 25;
+	double y = 35;
+
+	x > y ? printf("yes; x > y\n"): printf("no; y > x\n");
+
+	/* <condition>  ? <if block of the code> : <else if part> ; */
+
+	return 0;
+}
