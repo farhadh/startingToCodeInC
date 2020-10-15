36574c8b4ee9257b680316368b3da3b9f310d848
Farhad <Farhad.21.7@gmail.com>
2020-10-15 16:06:26 +0200

    Nested for loops

---
+++ nestedForLoops.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+	int i,j;
+	
+	for(i = 0; i<=10; i++){
+		for(j=i; j>=0; j--){
+			printf("%i ", j);
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
