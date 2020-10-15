1023cd1b17e140ce02d949b77394b0156d3d353a
Farhad <Farhad.21.7@gmail.com>
2020-10-15 16:06:26 +0200

    Nested for loops

--- nestedForLoops.c
+++ nestedForLoops.c
@@ -6,8 +6,13 @@
 
 int main() {
 	int i,j;
+<<<<<<< HEAD
 	
 	for(i = 0; i<=10; i++){
+=======
+
+	for(i = 0; i<10; i++){
+>>>>>>> 4f1f602... Nested for loops
 		for(j=i; j>=0; j--){
 			printf("%i ", j);
 		}
