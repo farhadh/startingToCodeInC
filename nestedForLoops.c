c0c4b158b014692f653498051ee4710baa62995a
Farhad <Farhad.21.7@gmail.com>
2020-10-16 10:33:30 +0200

    Cleared git conflict messages from the code
    I have no idea how those messages got into the code. I gotta find out.

--- nestedForLoops.c
+++ nestedForLoops.c
@@ -6,13 +6,8 @@
 
 int main() {
 	int i,j;
-<<<<<<< HEAD
 	
 	for(i = 0; i<=10; i++){
-=======
-
-	for(i = 0; i<10; i++){
->>>>>>> 4f1f602471cc6c51e16e93dcf82f1d110a2e3d37
 		for(j=i; j>=0; j--){
 			printf("%i ", j);
 		}
