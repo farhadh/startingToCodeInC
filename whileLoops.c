7a8643da7ef1bd7e501c151895fb99b65eade8c1
Farhad <Farhad.21.7@gmail.com>
2020-10-19 16:03:10 +0200

    While loops and nested while loops

---
+++ whileLoops.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+	
+	int i = 0, k;
+
+	while (i<=10) {	
+		
+		k = i-1;
+		
+		printf("%d ", i);
+		i++;
+		
+		while(k>=0){
+			printf("%d ", k);	
+			k--;
+		}
+		
+		printf("\n");
+	
+	}
+
+	return 0;
+}
