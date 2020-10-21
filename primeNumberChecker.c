551c9d39f5645969238297dc2bece32fe3d31098
Farhad <Farhad.21.7@gmail.com>
2020-10-21 12:03:59 +0200

    Revert "Revert "Added the mathematical logic behind the algorithm.""
    This reverts commit 6d118bf6

--- primeNumberChecker.c
+++ primeNumberChecker.c
@@ -1,3 +1,10 @@
+/*
+The logic behind the square root of the number under the test:
+
+If 𝑎.𝑏=𝑁 where 1 < 𝑎 ≤ 𝑏 < 𝑁
+𝑁 = 𝑎.𝑏 ≥ 𝑎^2 ⟺  𝑎^2 ≤ 𝑁 ⟹  𝑎 ≤ √𝑁 
+*/
+
 #include <stdio.h>
 #include <math.h>
 
@@ -7,8 +14,8 @@ int main() {
 	printf("Please enter the number you'd like to know whether it's a prime number or not: ");
 	scanf("%ld", &inputNumber);
 	
-	unsigned long highLimit = sqrt(inputNumber);
-	for(i=2; i<=highLimit+1;i++){
+	unsigned long highLimit = sqrt(inputNumber)+1;
+	for(i=2; i<=highLimit;i++){
 		remainder = inputNumber % i;
 		if (inputNumber != 2 && remainder==0){
 			primeNumFlag=0;
